8bf0409ad7b6df51cec0bfb10dce294ab51aa7eb
adildulobu <adildulobu@gmail.com>
2020-08-06 21:59:28 +0200

    tirei a parte do caminho do link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,8 +25,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="./img/supermarket.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="#" class="brand-link">
+      <img src="./img/supermarket.png" alt="Mercearia logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Mercearia</span>
     </a>
